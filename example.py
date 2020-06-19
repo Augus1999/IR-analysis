d0d7dc186c9ef75c6b877436c46c331b29a07e34
Phoenix AUGUS <39725660+Augus1999@users.noreply.github.com>
2020-06-19 20:38:28 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from infraredAnalysis import *
 
 
-a = data_open('sample.CSV')
+a = data_open('sample and results\\sample.CSV')
 b = peak_find(a, 0.5, 50)
 c = quick_peak_classify(b)
 plot(set1=a, set2=c, title='sample', save=True)
